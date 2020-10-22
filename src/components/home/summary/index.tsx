d0252b2f8798fe2d7951f6830d41204372a9218c
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-22 21:15:51 +0200

    Restyle home page

--- src/components/home/summary/index.tsx
+++ src/components/home/summary/index.tsx
@@ -5,17 +5,17 @@ import {
   summaryHeading,
   summaryText,
 } from "../../../content/text/summary";
-import Card from "../card";
 import presets from "./summary.module.scss";
 import SummaryProps from "./summary.props";
 
 const Summary: React.FC<SummaryProps> = (props) => {
+  const { preset = "default", ...rest } = props;
   return (
-    <Card {...props}>
+    <div className={presets[preset]} {...rest}>
       <p className={presets.heading}>{summaryHeading}</p>
       <p>{summaryText}</p>
       <p className={presets.finisher}>{summaryFinisher}</p>
-    </Card>
+    </div>
   );
 };
 
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+import CV from "../../components/home/cv";
 import Header from "../../components/home/header";
+import Summary from "../../components/home/summary";
 import Screen from "../../components/screen";
+import presets from "./home.module.scss";
 
 const Home: React.FC = () => {
   return (
     <Screen>
       <Header />
+      <div className={presets.container}>
+        <Summary />
+        <div className={presets.cv_container}>
+          <CV />
+        </div>
+      </div>
     </Screen>
   );
 };
