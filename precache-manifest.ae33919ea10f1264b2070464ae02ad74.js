41238ebb1f5d1bcba05bfcefd1010cb5b10074a0
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-23 00:57:55 +0200

    Updates

---
+++ precache-manifest.ae33919ea10f1264b2070464ae02ad74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3d3991a2de077d9660c36fd14777a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8eba4ede587ad9e355",
+    "url": "/static/css/main.0e0ef9d1.chunk.css"
+  },
+  {
+    "revision": "675fa9df72e8898f985c",
+    "url": "/static/js/2.14a3387f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.14a3387f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8eba4ede587ad9e355",
+    "url": "/static/js/main.5f007634.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "a55b83950a6042880fdfe967728f0de3",
+    "url": "/static/media/portrait.a55b8395.jpeg"
+  }
+]);
