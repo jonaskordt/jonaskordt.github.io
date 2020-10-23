e19dbe593b90f57d301fe99365e3eeacf283784a
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-23 18:24:21 +0200

    Add projects container to home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import CVCard from "../../components/home/cvCard";
 import Header from "../../components/home/header";
+import Heading from "../../components/home/heading";
 import Summary from "../../components/home/summary";
 import Screen from "../../components/screen";
 import presets from "./home.module.scss";
@@ -10,13 +11,20 @@ const Home: React.FC = () => {
   return (
     <Screen>
       <Header />
-      <div className={presets.container}>
+      <div className={presets.upper_container}>
         <Summary />
         <div className={presets.cv_container}>
           <CVCard />
         </div>
       </div>
-      <p>Projects coming soon...</p>
+      <div className={presets.lower_container}>
+        <div>
+          <Heading text="Projects" />
+        </div>
+        <div>
+          <p>Projects coming soon...</p>
+        </div>
+      </div>
     </Screen>
   );
 };
