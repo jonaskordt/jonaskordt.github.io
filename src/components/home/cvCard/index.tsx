8a3a74023dbccbdc2286f4ca154e071132e79d60
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-27 15:53:41 +0100

    Use img for cv card

--- src/components/home/cvCard/index.tsx
+++ src/components/home/cvCard/index.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 
 import Card from "../card";
+import cvImg from "./cv.png";
 import presets from "./cvCard.module.scss";
 import CVProps from "./cvCard.props";
 
 const CVCard: React.FC<CVProps> = (props) => {
   return (
     <Card to="/cv" {...props}>
-      <div className={presets.container}>
-        <p className={presets.c}>C</p>
-        <p className={presets.v}>V</p>
-        <p className={presets.urriculum}>urriculum</p>
-        <p className={presets.itae}>itae</p>
-      </div>
+      <img className={presets.cvImg} src={cvImg} alt="" />
       <p className={presets.coHeader}>Current Occupation:</p>
       <p>M.Sc. IT Systems Engineering at HPI</p>
     </Card>
