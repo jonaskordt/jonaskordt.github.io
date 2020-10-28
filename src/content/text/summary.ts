612b8c10138ce553b6f8766cf0ff272f33b243e6
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-28 14:18:39 +0100

    Extend summary

--- src/content/text/summary.ts
+++ src/content/text/summary.ts
@@ -5,7 +5,7 @@ export const summaryText = `
 I am a young and active software engineer from Berlin, Germany. 
 After completing my Bachelor's degree in IT Systems Engineering at HPI I am now working on my Master's degree.
 My main interests are computer graphics and machine learning.
-However, I also like to dabble with human computer interaction and UI design.`;
+However, I also like to dabble with human computer interaction and UI design and have quite some experience with web development.`;
 
 export const summaryFinisher = `
 Feel free to check out some of my projects below.`;
