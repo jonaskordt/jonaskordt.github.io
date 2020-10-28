bbc113715862b06a08ac75c1209eee8261a77bb0
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-10-28 13:18:13 +0100

    Simplify dates in cv content

--- src/content/cv/scholarschips.ts
+++ src/content/cv/scholarschips.ts
@@ -4,7 +4,7 @@ const scholarships: CVEntryData[] = [
   {
     heading: "Talent Program",
     organization: "Friedrich Naumann Stiftung für die Freiheit",
-    start: new Date(2017, 9, 1),
+    start: new Date(2017, 9),
     organizationLink: "https://www.freiheit.org",
   },
 ];
