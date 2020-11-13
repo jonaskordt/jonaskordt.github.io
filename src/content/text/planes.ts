a8cd5b254402fad6eb36f6597afecce8458cac58
Jonas Kordt <jonas.kordt@student.hpi.de>
2020-11-13 18:50:13 +0100

    Improve planes quick summary

--- src/content/text/planes.ts
+++ src/content/text/planes.ts
@@ -2,4 +2,4 @@ export const planesSummary =
   "In computer graphics it is usually not possible to see object A through object B if you can also see object B through object A. This usually is not a problem because if said object A is further away from the camera than object B, it is not even desirable to see object B through object A. However, this changes once the objects intersect like these three planes. Now, every plane should be able to shine through every other plane. To make this possible, I split the three planes at their intersections, which results in 12 plane parts and then position them seamlessly. Together with a custom rendering order for those 12 slices based on the position of the camera, this produces the perfect transparency effect seen here.";
 
 export const planesQuickSummary =
-  "A way to have properly transparent intersecting planes with a variable intersection point. Part of Classifai 3D.";
+  "A way to have properly transparent intersecting planes with a variable intersection point. Part of Classifai’s 3D Toolkit.";
