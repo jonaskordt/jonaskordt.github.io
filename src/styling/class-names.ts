1b600583fd5d461a73e3a777b3b0697a249ef1ac
Jonas Kordt <jonas.kordt@student.hpi.de>
2022-01-30 16:58:26 +0100

    Remove unused code

--- src/styling/class-names.ts
+++
@@ -1,6 +0,0 @@
-export const classNames = (
-  ...appliedClassNames: (string | false | undefined)[]
-) => {
-  const actualClassNames = appliedClassNames.filter((value) => Boolean(value));
-  return actualClassNames.length ? actualClassNames.join(" ") : undefined;
-};
