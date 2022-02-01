9f8b524a15b190ea78947db580f86e52a22270ff
Jonas Kordt <jonas.kordt@student.hpi.de>
2022-02-01 20:48:36 +0100

    Update summary

--- src/content/text/summary.tsx
+++ src/content/text/summary.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 export const summaryHeading = `
-Me in a Nutshell`;
+About Me`;
 
 export const summaryText = (
   <p>
-    I am a young and active software engineer from Berlin, Germany. After
-    completing my Bachelor&apos;s degree in IT Systems Engineering at HPI I am
-    now working on my Master&apos;s degree. My main interests are computer
-    graphics and machine learning. However, I also like to dabble with human
-    computer interaction and UI design and have quite some experience with web
-    development. Next to my Master&apos;s degree I am currently continuing
-    development of{" "}
+    I am a software engineer from Berlin, Germany. After completing my
+    Bachelor&apos;s degree in IT Systems Engineering at HPI I am now working on
+    my Master&apos;s degree. My main interests are computer graphics and machine
+    learning. However, I also like to dabble with human computer interaction and
+    UI design and have quite some experience with web development. Next to my
+    Master&apos;s degree I am currently working at{" "}
+    <a href="https://immunkarte.de" target="_blank" rel="noopener noreferrer">
+      Immunkarte
+    </a>{" "}
+    and continuing development of{" "}
     <a href="https://visian.org" target="_blank" rel="noopener noreferrer">
       VISIAN
     </a>{" "}
-    (formerly Classifai) together with a few other students and the chair for
-    Digital Health and Machine Learning at HPI in order to bring the system to
-    users.
+    at the chair for Digital Health and Machine Learning at HPI.
   </p>
 );
 
 export const summaryFinisher = `
-Feel free to check out some of my projects including Classifai below.`;
+Feel free to check out some of my projects below.`;
