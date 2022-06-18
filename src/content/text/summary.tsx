bb2786dc48a2e01a84204617c6a407067f28cda7
Jonas Kordt <jonas.kordt@student.hpi.de>
2022-06-18 18:09:42 +0200

    Update summary

--- src/content/text/summary.tsx
+++ src/content/text/summary.tsx
@@ -10,15 +10,16 @@ export const summaryText = (
     my Master&apos;s degree. My main interests are computer graphics and machine
     learning. However, I also like to dabble with human computer interaction and
     UI design and have quite some experience with web development. Next to my
-    Master&apos;s degree I am currently working at{" "}
-    <a href="https://immunkarte.de" target="_blank" rel="noopener noreferrer">
-      Immunkarte
-    </a>{" "}
-    and continuing development of{" "}
+    Master&apos;s degree I continued development of{" "}
     <a href="https://visian.org" target="_blank" rel="noopener noreferrer">
       VISIAN
     </a>{" "}
-    at the chair for Digital Health and Machine Learning at HPI.
+    at the chair for Digital Health and Machine Learning at HPI and I am now
+    working at{" "}
+    <a href="https://immunkarte.de" target="_blank" rel="noopener noreferrer">
+      Immunkarte
+    </a>
+    .
   </p>
 );
 
